147501909f02f504ed63794609db4bde39816b45
nickholt <NickHolt93@gmail.com>
2014-03-14 17:46:56 -0700

    Basic Player.java structure

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Player {
+	private int mScore;
 
+	public Player() {
+		setScore(0);
+	}
+
+	public int getScore() {
+		return mScore;
+	}
+
+	public float getScoreAsPercentage(int maxScore) {
+		return (float) mScore / (float) maxScore;
+	}
+
+	public void setScore(int score) {
+		mScore = score;
+	}
+	
+	public void updateScore(int updateAmount) {
+		mScore += updateAmount;
+	}
 }
